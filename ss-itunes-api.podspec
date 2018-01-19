28ddcd35504ea073ff596b17fd5312bc74c869ef
wansong <betterofsong@gmail.com>
2018-01-19 11:36:05 +0800

    0.1.1: no-cache

--- ss-itunes-api.podspec
+++ ss-itunes-api.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ss-itunes-api'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'A short description of ss-itunes-api.'
 
 # This description is used to generate tags and improve search results.
--- ss-itunes-api/Classes/SSItunesApi.m
+++ ss-itunes-api/Classes/SSItunesApi.m
@@ -9,7 +9,24 @@
 
 static NSString *ITUNES_API_BASE = @"https://itunes.apple.com";
 
+@interface SSItunesApi ()
+@property (strong, nonatomic) NSURLSession *urlSession;
+@end
+
 @implementation SSItunesApi
+
++ (instancetype) sharedInstance {
+  static dispatch_once_t onceToken;
+  static SSItunesApi *ssia = nil;
+  dispatch_once(&onceToken, ^{
+    ssia = [SSItunesApi new];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    config.requestCachePolicy = NSURLRequestReloadIgnoringCacheData;
+    NSURLSession *ssesion = [NSURLSession sessionWithConfiguration:config];
+    ssia.urlSession = ssesion;
+  });
+  return ssia;
+}
 
 + (void) getAppMetaAppId:(NSString *)appId
                   result:(NullableDictCallback)callback {
@@ -25,9 +42,11 @@ static NSString *ITUNES_API_BASE = @"https://itunes.apple.com";
   
   NSString *urlStr = [self lookUpUriWithQueries:queries];
   NSURL *url = [NSURL URLWithString:urlStr];
-  
-  NSURLSession *session = [NSURLSession sharedSession];
-  NSURLSessionTask *task = [session dataTaskWithURL:url
+  NSMutableURLRequest *rq = [NSMutableURLRequest requestWithURL:url];
+  [rq setValue:@"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+forHTTPHeaderField:@"User-Agent"];
+  NSURLSession *session = [[self sharedInstance] urlSession];
+  NSURLSessionTask *task = [session dataTaskWithRequest:rq
          completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
            if (error || !data) {
              NSLog(@"url load failed: url(%@), error(%@)", url, error);
@@ -48,6 +67,7 @@ static NSString *ITUNES_API_BASE = @"https://itunes.apple.com";
            NSDictionary *json = [ret isKindOfClass:NSDictionary.class] ? ret : nil;
            NSArray *results = [json[@"results"] isKindOfClass:NSArray.class] ? json[@"results"] : nil;
            NSDictionary *meta = (NSDictionary *)[results firstObject];
+           NSLog(@"appstore meta: %@", meta);
            callback([meta isKindOfClass:NSDictionary.class] ? meta : nil);
          }];
   [task resume];
